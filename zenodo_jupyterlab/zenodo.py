ba0344494353172979bb3357e4cba77c1f729b97
ejuet <lispam7989@gmail.com>
2026-06-17 17:17:05 +0200

    refactor: extract hardcoded url to global variable for now

--- zenodo_jupyterlab/zenodo.py
+++ zenodo_jupyterlab/zenodo.py
@@ -2,9 +2,12 @@ from urllib.error import HTTPError, URLError
 from urllib.request import Request, urlopen
 
 
+ZENODO_SERVER_URL = "https://zenodo.org"
+
+
 def is_zenodo_access_token_valid(access_token: str) -> bool:
     request = Request(
-        "https://zenodo.org/api/deposit/depositions",
+        f"{ZENODO_SERVER_URL}/api/deposit/depositions",
         headers={"Authorization": f"Bearer {access_token}"},
     )
     try:
