7bed61eb8785ddcdf737d8dca5a141dd290599fc
ejuet <lispam7989@gmail.com>
2026-08-04 15:37:58 +0200

    refactor: only show relevant buttons for login menu

--- src/components/ZenodoLoginForm.tsx
+++ src/components/ZenodoLoginForm.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
 
-import { AuthButtons } from './AuthButtons';
-import { LoginStatusPill } from './LoginStatusPill';
+import { LoginButton, LogoutButton } from './AuthButtons';
 import { ZenodoUserProfile } from './ZenodoUserProfile';
+import { useAccessTokenStatus } from '../api_calls';
 
 export const ZenodoLoginForm: React.FC = () => {
+  const accessStatus = useAccessTokenStatus();
+
+  if (!accessStatus) {
+    return <span>Loading...</span>;
+  }
+
+  const loggedIn =
+    accessStatus &&
+    accessStatus.access_token_present &&
+    accessStatus.access_token_valid;
+
   return (
     <>
-      <LoginStatusPill />
-      <AuthButtons sandbox={false} />
-      <hr />
-      <ZenodoUserProfile />
+      {!loggedIn && (
+        <div>
+          <p>Log in to Zenodo to access your account and upload records.</p>
+          <LoginButton sandbox={false} />
+        </div>
+      )}
+      {loggedIn && (
+        <div>
+          <ZenodoUserProfile />
+          {accessStatus.sandbox && (
+            <p>You are logged in to the sandbox environment.</p>
+          )}
+          <LogoutButton sandbox={false} />
+        </div>
+      )}
     </>
   );
 };
