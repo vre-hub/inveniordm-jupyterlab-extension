c0ab435db081315b11f63692d532d29dd8f67f1b
ejuet <lispam7989@gmail.com>
2026-08-05 10:28:40 +0200

    style: fix always having horizontal overflow

--- src/widgets/SidebarPanel.tsx
+++ src/widgets/SidebarPanel.tsx
@@ -73,7 +73,7 @@ const Panel: React.FC = () => {
       />
       <div
         aria-labelledby={`tab-${selectedTab.id}`}
-        className="min-h-0 flex-1 overflow-auto p-3"
+        className="min-h-0 flex-1 overflow-y-auto p-3"
         id={TAB_PANEL_ID}
         role="tabpanel"
         tabIndex={0}
